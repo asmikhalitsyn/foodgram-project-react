263e4d139cc06d0f19875be160a6c0f1da458155
Andrey Mikhalitsyn <andreymikh92@gmail.com>
2022-11-20 23:48:06 +0300

    Спринт 14. Коммит13

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,10 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from .settings import (
-    EMAIL_MAX_LENGHT,
-    NAME_MAX_LENGHT
-)
+from .settings import EMAIL_MAX_LENGHT, NAME_MAX_LENGHT
 
 
 class User(AbstractUser):
