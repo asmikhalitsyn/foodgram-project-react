8c01e1fa484e4f42ee51f726281424c96090853f
Andrey Mikhalitsyn <andreymikh92@gmail.com>
2022-11-26 17:18:18 +0300

    testtest12

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,4 @@
 from djoser.serializers import UserSerializer, UserCreateSerializer
-from rest_framework import serializers
 from rest_framework.serializers import (
     ModelSerializer,
     PrimaryKeyRelatedField,
@@ -24,8 +23,14 @@ from users.models import Follow, User
 class CustomUserCreateSerializer(UserCreateSerializer):
     class Meta:
         model = User
-
-        fields = ('email', 'id', 'username', 'first_name', 'last_name', 'password')
+        fields = (
+            'email',
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'password'
+        )
 
 
 class UserSerializer(UserSerializer):
