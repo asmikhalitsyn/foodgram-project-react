61ee80eab08363436b2f296e43d99cedf22dbe7e
Andrey Mikhalitsyn <andreymikh92@gmail.com>
2022-12-02 23:51:45 +0300

    final1992347799335

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -11,7 +11,6 @@ from rest_framework import permissions, status
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.permissions import SAFE_METHODS
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
