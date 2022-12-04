eecdd73a9cfe11937a628daa136b788deb0277f4
Andrey Mikhalitsyn <andreymikh92@gmail.com>
2022-12-04 13:41:37 +0300

    Followtest

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -115,7 +115,7 @@ DJOSER = {
     'HIDE_USERS': False,
     'LOGIN_FIELD': 'email',
     'PERMISSIONS': {
-        'user': ['rest_framework.permissions.IsAuthenticated'],
+        'user': ['rest_framework.permissions.AllowAny'],
         'user_list': ['rest_framework.permissions.AllowAny'],
 
     },
