1875c26fedd5e31cdd5910e0dde6b611b67f13b0
Andrey Mikhalitsyn <andreymikh92@gmail.com>
2022-12-04 14:02:43 +0300

    Followtest1234

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,7 +2,6 @@ from django.urls import include, path, re_path
 from rest_framework import routers
 
 from .views import (
-    CustomTokenCreateView,
     IngredientViewSet,
     RecipeViewSet,
     TagViewSet,
