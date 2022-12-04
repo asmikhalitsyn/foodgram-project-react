2c40f33917ca5a3db2a0ad78c4181d617079bcd4
Andrey Mikhalitsyn <andreymikh92@gmail.com>
2022-12-04 14:42:42 +0300

    Followtest123456

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -45,11 +45,13 @@ class UserSerializer(UserSerializer):
         )
 
     def get_is_subscribed(self, obj):
-        request = self.context.get('request')
-        if not request or request.user.is_anonymous:
+        user = self.context.get('request').user
+        if user.is_anonymous:
             return False
         return Follow.objects.filter(
-            user=request.user, following=obj).exists()
+            author=obj,
+            user=user
+        ).exists()
 
 
 class TagSerializer(ModelSerializer):
